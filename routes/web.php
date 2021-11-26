377c1cd0574b6f167fa2dbe9e2b7fb2845166585
Nihal Goindi <nihal082000@gmail.com>
2021-11-26 23:13:06 +0000

    installed breeze again

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,6 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use app\Http\Controllers;
 
 /*
 |--------------------------------------------------------------------------
@@ -17,3 +16,9 @@ use app\Http\Controllers;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';

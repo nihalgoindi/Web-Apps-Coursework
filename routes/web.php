64228df6ee136b54579076f5e34af0c2e9b0de99
Nihal Goindi <nihal082000@gmail.com>
2021-11-26 16:56:36 +0000

    Routes for posts: index, create, store, show

--- routes/web.php
+++ routes/web.php
@@ -21,4 +21,16 @@ Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
 
+Route::get('/posts', [PostController::class, 'index'])
+    ->name('posts.index');
+
+Route::get('/posts/create', [PostController::class, 'create'])
+    ->name('posts.create');
+
+Route::get('/posts/', [PostController::class, 'store'])
+    ->name('posts.store');
+
+Route::get('/posts/{id}', [PostController::class, 'show'])
+    ->name('posts.show');
+
 require __DIR__.'/auth.php';

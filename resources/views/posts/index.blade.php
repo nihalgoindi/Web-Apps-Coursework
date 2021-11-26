b08c771c3e7d359dd2f7fa9a7cfb5fb7360adc9c
Nihal Goindi <nihal082000@gmail.com>
2021-11-26 16:59:29 +0000

    posts index blade

---
+++ resources/views/posts/index.blade.php
@@ -0,0 +1,20 @@
+@extends('layouts.app')
+
+@section('title')
+    Posts
+@endsection
+
+@section('content')
+    <p>The Posts within this Community:</p>
+    <ul>
+        @foreach ($posts as $post)
+            <li> <a href="{{ route('posts.show', [ 'id' => $post->id ] )}}">{{$post->name}}</a></li>
+        @endforeach
+    </ul>
+
+    <a href="{{ route('posts.create') }}">Create Post</a>
+@endsection
+
+@if (session('message'))
+    <p><b>{{ session('message') }}</b></p>
+@endif

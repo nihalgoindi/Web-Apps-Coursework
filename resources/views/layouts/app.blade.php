22cf4fd5614554b96c20145c9d300e56e76f9d8f
Nihal Goindi <nihal082000@gmail.com>
2021-12-02 17:32:00 +0000

    Added a template

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,37 +1,86 @@
-<!DOCTYPE html>
 <html lang="en">
-<head>    
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
-    <meta http-equiv="X-UA-Compatible" content="ie-edge">
-    <title>Swansea Zoo - @yield('title')</title>
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Laravel Quickstart - Intermediate</title>
+
+    <!-- Fonts -->
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel='stylesheet' type='text/css'>
+    <link href="https://fonts.googleapis.com/css?family=Lato:100,300,400,700" rel='stylesheet' type='text/css'>
+
+    <!-- Styles -->
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" rel="stylesheet">
+    {{-- <link href="{{ elixir('css/app.css') }}" rel="stylesheet"> --}}
+
+    <style>
+        body {
+            font-family: 'Lato';
+        }
+        .fa-btn {
+            margin-right: 6px;
+        }
+    </style>
 </head>
-<body>
-    <h1>Swansea Zoo - @yield('title')</h1>
+<body id="app-layout">
+   <div class="container-fluid">
+      <!-- First section -->
+      <nav class="navbar navbar-dark bg-dark">
+        <div class="container">
+          <h1>
+            <a href="#" class="navbar-brand">ThoughtSpace Forums</a>
+          </h1>
+          <form action="#" class="form-inline mr-3 mb-2 mb-sm-0">
+            <input type="text" class="form-control" placeholder="search" />
+            <button type="submit" class="btn btn-success">Search Forum</button>
+          </form>
+        </div>
+      </nav>
 
-    @if ($errors->any())
-    <div>
-        Errors:
-
-        <ul>
-            @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>     
-            @endforeach
-        </ul>
+      <!-- first section end -->
     </div>
-@endif
+    <div class="container">
+      <nav class="breadcrumb">
+        <a href="#" class="breadcrumb-item active"> Dashboard</a>
+ {{--      </nav>
+                    <!-- Authentication Links -->
+                    @if (Auth::guest())
+                        <li><a href="{{ url('/login') }}">Login</a></li>
+                        <li><a href="{{ url('/register') }}">Register</a></li>
+                    @else
+                        <li class="dropdown">
+                            <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+                                {{ Auth::user()->name }} <span class="caret"></span>
+                            </a>
 
-    <div>
-        @yield('content')
+                            <ul class="dropdown-menu" role="menu">
+                                <li><a href="{{ url('/logout') }}"><i class="fa fa-btn fa-sign-out"></i>Logout</a></li>
+                            </ul>
+                        </li>
+                    @endif
+                </ul>
+            </div>
+        </div>
+    </nav> --}}
+
+    @yield('content')
+    <div class="container-fluid">
+      <footer class="small bg-dark text-white">
+        <div class="container py-4">
+          <ul class="list-inline mb-0 text-center">
+            <li class="list-inline-item">
+              &copy; 2021 ThoughtSpace Forums
+            </li>
+            <li class="list-inline-item">All rights reserved</li>
+            <li class="list-inline-item">Terms and privacy policy</li>
+          </ul>
+        </div>
+      </footer>
     </div>
-
-    <div>
-        <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();">Logout</a>
-
-        <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
-        @csrf
-</form>
-    </div>
+    <!-- JavaScripts -->
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/2.1.4/jquery.min.js"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    {{-- <script src="{{ elixir('js/app.js') }}"></script> --}}
 </body>
 </html>

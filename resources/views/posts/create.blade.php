a301427a7afd8f6aba827b1aab81f3c0da154387
Nihal Goindi <nihal082000@gmail.com>
2021-12-09 18:13:55 +0000

    create post blade, moved attachment button

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,7 +1,5 @@
 @extends('layouts.app')
 
-@extends('layouts.app')
-
 @section('content')
 <div class="w-4/5 m-auto text-left">
     <div class="pb-10 ">
@@ -11,30 +9,37 @@
     </div>
 </div>
 
-<div class="w-4/5 m-auto pt-20">
-    <form method="POST" action="{{ route('posts.store') }}" enctype="multipart/form-data">
+<div class="w-4/5  m-auto pt-20">
+    <form method="POST" action="{{ route('posts.store') }}" enctype="multipart/form-data" class="space-y-5">
         @csrf
+
+        <label class="flex-shrink-0  bg-blue-500 hover:bg-blue-600 uppercase bg-transparent text-gray-100 text-lg 
+        font-extrabold py-3.5 px-5 rounded-3xl">
+            <span class="mt-2 text-base leading-normal">
+                Upload a file
+            </span>
+            <input type="file" name="image" class="hidden">
+        </label>
         
         <input type="text" name="title" placeholder="Title..." 
-        class="bg-transparent block border-b-2 w-full h-20 text-6xl outline-none">
+        class="bg-transparent block border-none w-full h-20 text-gray-700 text-6xl p-8">
 
-        <textarea name="Body" placeholder="Body"
-        class="py-20 bg-transparent block border-b-2 w-full h-60 text-xl outline-none"></textarea>
+        <textarea name="Body" placeholder=" Body"
+        class="py-20 bg-transparent block border-none w-full h-60 text-gray-700 text-xl"></textarea>
 
         <div class="bg-grey-lighter pt-15">
-            <label class="w-44 flex flex-col items-center px-2 py-3 bg-white-rounded-lg shadow-lg 
-            tracking-wide uppercase border border-blue cursor-pointer">
-                <span class="mt-2 text-base leading-normal">
-                    Upload a file
-                </span>
-                <input type="file" name="image" class="hidden">
-            </label>
-
-            <button type="submit" class="uppercase mt-15 bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
+            <button type="button" class="flex-shrink-0  bg-blue-500 hover:bg-blue-600 uppercase bg-transparent text-gray-100 text-lg 
+            font-extrabold py-3 px-11 rounded-3xl">
+                Cancel
+            </button>
+            <button type="submit" class="flex-shrink-0  bg-blue-500 hover:bg-blue-600 uppercase bg-transparent text-gray-100 text-lg 
+            font-extrabold py-3 px-5 rounded-3xl">
                 Submit Post
             </button>
+            
         </div>
         
+       
 
     
     </form>

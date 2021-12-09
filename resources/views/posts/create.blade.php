c39952a4a90b221a3bc57eedd9fa3d85b945e82c
Nihal Goindi <nihal082000@gmail.com>
2021-12-09 16:05:40 +0000

    added some css to create

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -1,25 +1,43 @@
 @extends('layouts.app')
 
-@section('title')
-    Create Post
-@endsection()
+@extends('layouts.app')
 
 @section('content')
-    <form method="POST" action="{{ route('posts.store') }}">
+<div class="w-4/5 m-auto text-left">
+    <div class="pb-10 ">
+        <h1 class="text-6xl">
+            Create Post
+        </h1>
+    </div>
+</div>
+
+<div class="w-4/5 m-auto pt-20">
+    <form method="POST" action="{{ route('posts.store') }}" enctype="multipart/form-data">
         @csrf
+        
+        <input type="text" name="title" placeholder="Title..." 
+        class="bg-transparent block border-b-2 w-full h-20 text-6xl outline-none">
 
-        <p>Title: <input type="text" name="name"
-            value="{{ old('name') }}"></p>
+        <textarea name="Body" placeholder="Body"
+        class="py-20 bg-transparent block border-b-2 w-full h-60 text-xl outline-none"></textarea>
 
-        <p>Body: <input type="text" name="weight"
-            value="{{ old('weight') }}"></p>
+        <div class="bg-grey-lighter pt-15">
+            <label class="w-44 flex flex-col items-center px-2 py-3 bg-white-rounded-lg shadow-lg 
+            tracking-wide uppercase border border-blue cursor-pointer">
+                <span class="mt-2 text-base leading-normal">
+                    Upload a file
+                </span>
+                <input type="file" name="image" class="hidden">
+            </label>
 
-        <p>Account Id: <input type="text" name="account_id"
-            value="{{ old('account_id') }}"></p>
+            <button type="submit" class="uppercase mt-15 bg-blue-500 text-gray-100 text-lg font-extrabold py-4 px-8 rounded-3xl">
+                Submit Post
+            </button>
+        </div>
+        
 
-        <input type="submit" value="Submit">
-
-        <a href="{{ route('posts.index') }}">Cancel</a>
-      
+    
     </form>
+</div>
+
 @endsection()

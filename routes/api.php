b81f3698a93e4032c40700f991539efeb7a90015
Nihal Goindi <nihal082000@gmail.com>
2021-12-15 10:02:29 +0000

    moving the comments section to its own blade

--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\CommentController;
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +14,12 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::get('/enclosures', [EnclosureController::class, 'apiIndex'])
+    ->name('api.enclosures.index');
+
+Route::post('/enclosures', [EnclosureController::class,'apiStore'])
+    ->name('api.enclosures.store');
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();

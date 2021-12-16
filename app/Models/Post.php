74a909a360519ada09c797f1e3da2ada4796933b
user name <user email>
2021-12-16 13:05:51 +0000

    post-tag many-to-many relationship

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -27,6 +27,10 @@ class Post extends Model
         return $this->hasMany(Comment::class);
     }
 
+    public function tags()
+    {
+        return $this->belongsToMany(Tag::class);
+    }
 
 }
 
